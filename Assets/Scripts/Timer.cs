dabd8802bf50fb0731273baf817a0100df3a794a
Pedro2901 <pedro290190@gmail.com>
2022-04-20 12:30:58 -0400

    Reloj añadido
    y personajes puestos en esquinas diferentes

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,80 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    private float timeDuration = 2f * 60f;
+
+    private float _time;
+
+    [SerializeField] private TextMeshProUGUI FirstMinute;
+    [SerializeField] private TextMeshProUGUI SecondMinute;
+    [SerializeField] private TextMeshProUGUI Separator;
+    [SerializeField] private TextMeshProUGUI FirstSecond;
+    [SerializeField] private TextMeshProUGUI SecondSecond;
+
+    private float flashtimer;
+    private float flashduration = 1f;
+    void Start()
+    {
+        ResetTimer();
+    }
+
+    
+    void Update()
+    {
+        if (_time > 0)
+        {
+            _time -= Time.deltaTime; //cada frame o actualizacion tomaremos el tiempo entre ellas 
+            UptadeTimerDisplay(_time);
+        }
+        else {
+            flash();
+        }
+        
+    }
+
+    private void ResetTimer() {
+        _time = timeDuration;
+    }
+
+    private void UptadeTimerDisplay(float time) {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+        string currentTime = string.Format("{00:00}{1:00}", minutes, seconds);
+        FirstMinute.text = currentTime[0].ToString();
+        SecondMinute.text = currentTime[1].ToString();
+        FirstSecond.text = currentTime[2].ToString();
+        SecondSecond.text = currentTime[3].ToString();
+    }
+    private void flash() {
+        if (_time != 0) {
+            _time = 0;
+            UptadeTimerDisplay(_time);
+        }
+        if (flashtimer <= 0)
+        {
+            flashtimer = flashduration;
+        }
+        else if (flashtimer >= flashduration / 2)
+        {
+            flashtimer -= Time.deltaTime;
+            SetTextDisplay(false);
+
+        }
+        else {
+            flashtimer -= Time.deltaTime;
+            SetTextDisplay(true);
+        }
+    }
+
+    private void SetTextDisplay(bool enabled) {
+        FirstMinute.enabled = enabled;
+        SecondMinute.enabled = enabled;
+        Separator.enabled = enabled;
+        FirstSecond.enabled = enabled;
+        SecondSecond.enabled = enabled;
+    }
+}
